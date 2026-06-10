ad158f43fb5fd5cf6927f909b7563caa462f49bd
rad <BebopByte@users.noreply.github.com>
2026-06-10 13:38:00 -0400

    (scanner) FEAT - generating and listing ip sets

--- scanner/main.go
+++ scanner/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
 	fmt.Println("Network Monitor Scanner Statring...")
+
+	subnet := os.Getenv("SUBNET")
+
+	for i := 1; i <= 254; i++ {
+
+		ip := generateIP(subnet, i)
+
+		fmt.Println(ip)
+
+	}
+}
+
+func generateIP(subnet string, host int) string {
+	return fmt.Sprintf("%s%d", subnet, host)
 }
