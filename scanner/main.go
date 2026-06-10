70903b6254990aa12059e18883d48f9e68251f35
rad <BebopByte@users.noreply.github.com>
2026-06-10 14:05:22 -0400

    (scanner) FEAT - implemented ping test method / added godotenv dependency

--- scanner/main.go
+++ scanner/main.go
@@ -2,24 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	fmt.Println("Network Monitor Scanner Statring...")
 
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	subnet := os.Getenv("SUBNET")
 
-	for i := 1; i <= 254; i++ {
+	for i := 1; i <= 20; i++ {
 
 		ip := generateIP(subnet, i)
 
-		fmt.Println(ip)
-
+		if ping(ip) {
+			fmt.Printf("%s is ONLINE\n", ip)
+		}
 	}
 }
 
 func generateIP(subnet string, host int) string {
 	return fmt.Sprintf("%s%d", subnet, host)
+}
+
+func ping(ip string) bool {
+
+	cmd := exec.Command("ping", "-n", "1", ip)
+
+	err := cmd.Run()
+
+	return err == nil
+
 }
