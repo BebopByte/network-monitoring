6ad24c0f15125589e65cb532f8033595707cfaa6
rad <BebopByte@users.noreply.github.com>
2026-06-14 14:43:19 -0400

    (services) init: created a new spring-boot project for backend api services

---
+++ services/monitor-api/src/main/java/com/networkmonitor/monitor_api/MonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.networkmonitor.monitor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApiApplication.class, args);
+	}
+
+}
